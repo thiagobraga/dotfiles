b0842131b8e5198cc2e4e3e3e8de12ee2f1f9407
Thiago Braga <thibraga06@gmail.com>
2026-03-28 18:56:52 -0300

    fix(utils): rename silent to allow set -x toggling and add trace function

--- .helpers/utils.sh
+++ .helpers/utils.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -euo pipefail
+set -e
 
 # ANSI color codes
 GREEN='\033[0;32m'
@@ -12,6 +12,19 @@ CYAN='\033[0;36m'
 NC='\033[0m' # No Color
 
 info() { printf "${GRAY}[info]${NC} %s\n" "${1:-}"; }
-success() { printf "${GREEN}done${NC} %s\n\n" "${1:-}"; }
+success() { printf "${GREEN}[done]${NC} %s\n\n" "${1:-}"; }
 warning() { printf "${YELLOW}[warning]${NC} %s\n\n" "${1:-}"; }
 error() { printf "${RED}[error]${NC} %s\n\n" "${1:-}"; }
+
+silent() {
+  { set +x; } 2>/dev/null
+  "$@"
+  set -x
+}
+
+# Run a single command with tracing (set -x) ON, then disable tracing afterward.
+trace() {
+  set -x
+  "$@"
+  { set +x; } 2>/dev/null
+}
